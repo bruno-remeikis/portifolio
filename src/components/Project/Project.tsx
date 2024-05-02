c709295e2ff79d75103001f16b47bd7262e70fb9
= <brunocoutinhoremeikis@gmail.com>
2024-05-02 17:49:29 -0300

    Adds "Marvel Tic Tac Toe" project / expand projects button.

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,170 @@
+import Image from 'next/image';
+import styles from './Project.module.scss';
+import { HiMiniCodeBracket, HiMiniXMark, HiMiniGlobeAlt } from "react-icons/hi2";
+import { AiOutlineFullscreen } from "react-icons/ai";
+import { useState } from 'react';
+import { isDesktop } from 'react-device-detect';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Link from 'next/link';
+import { Technology } from '../../utils/technologies';
+import { Modal } from '../Modal';
+
+type ImageProps = {
+    src: string;
+    size: number[]; // [0]: width; [1]: height
+}
+
+type ProjectProps = {
+    img: string;
+    name: string;
+    demoLink?: string;
+    githubLink?: string;
+
+    imgAlign?: 'center' | 'top' | 'bottom' | number;
+    mobile?: boolean; // Na vitrine, imagens ocupam duas linhas e, no modal, imagens ficam na lateral
+    butDesktopShowcase?: boolean; // Se mobile=true e butDesktopShowcase=true, não exibe a imagem da vitrine em duas linhas
+
+    // For modal
+    imgs: ImageProps[];
+    description: string;
+    technologies: Technology[];
+    intention?: string;
+
+    lastVisibleOne?: boolean;
+}
+
+export const Project: React.FC<ProjectProps> = ({
+    img, name, demoLink, githubLink, imgAlign = 'center', mobile, butDesktopShowcase, imgs, description, technologies, intention, lastVisibleOne = false
+}) =>
+{
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [closing, setClosing] = useState<boolean>(false);
+
+    function requestClose()
+    {
+        setClosing(true);
+
+        setTimeout(() => {
+            setClosing(false);
+            setIsOpen(false);
+        }, 300);
+    }
+
+    const getImgAlign = () =>
+        imgAlign === 'center' ? '50%' :
+        imgAlign === 'top' ? '0%' :
+        imgAlign === 'bottom' ? '100%' :
+        `${imgAlign}%`;
+
+    return (
+        <div
+            className={`${styles.project} ${isDesktop ? styles.project__isDesktop : ''} ${lastVisibleOne ? styles.lastVisibleOne : ''}`}
+            style={
+                mobile && !butDesktopShowcase ? { gridRow: 'span 2' } : null
+            }
+        >
+            <div className={styles.imgContainer}>
+                <div className={styles.img} style={{
+                    backgroundImage: `url('/img/projects/${img}')`,
+                    backgroundPositionX: '50%',
+                    backgroundPositionY: getImgAlign()
+                }} />
+            </div>
+
+            <div
+                className={styles.caption}
+                onClick={() => setIsOpen(true)}
+            >
+                <span>{ name } <AiOutlineFullscreen /></span>
+                <span>Clique para ver detalhes</span>
+            </div>
+
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={requestClose}
+                overlayClass={`${styles.modalOverlay} ${mobile ? styles.modalOverlay__Mobile : ''} ${closing ? styles.modalOverlay__Closing : ''} ${isOpen ? styles.modalOverlay__Open : ''}`}
+                containerClass={styles.modalContainer}
+            >
+                <div className={styles.modalContent}>
+                    <div className={styles.carouselContainer}>
+                        <Carousel className={styles.carousel} showThumbs={false}>
+                            {imgs.map(img =>
+                                <Image
+                                    key={img.src}
+                                    className={styles.carousel__item}
+                                    src={`/img/projects/${img.src}`}
+                                    width={img.size[0] ? img.size[0] : 10}
+                                    height={img.size[1] ? img.size[1] : 10}
+                                    layout='intrinsic'
+                                    alt={`${name} image`}
+                                    title={`${name} image`}
+                                />
+                            )}
+                        </Carousel>
+                    </div>
+
+                    <div className={styles.data}>
+                        <div className={styles.header}>
+                            <h3>{ name }</h3>
+                            <div className={styles.technologies}>
+                                {technologies.map((t, i) =>
+                                    <Image
+                                        key={i}
+                                        src={`/img/abilities/${t.imageName}.${t.extension ? t.extension : 'svg'}`}
+                                        // layout='intrinsic'
+                                        width={35}
+                                        height={35}
+                                        alt={t.name}
+                                        title={t.name}
+                                    />    
+                                )}
+                            </div>
+                        </div>
+
+                        <div className={styles.modalInfo}>
+                            <div className={styles.description}>
+                                {description.split('\n').map((desc, i) =>
+                                    <p key={i}>{ desc }</p>
+                                )}
+                            </div>
+
+                            {intention ?
+                                <div className={styles.intention}>
+                                    <span>Intúito: </span>
+                                    <span>{ intention }</span>
+                                </div>
+                            : null}
+                        </div>
+
+                        <div className={styles.projectLinks}>
+                            <div>
+                                {demoLink ?
+                                    <Link href={demoLink} target='_blank' className={styles.demoLink}>
+                                        <HiMiniGlobeAlt />
+                                        Visitar
+                                    </Link>
+                                : null}
+
+                                {githubLink ? 
+                                    <Link href={githubLink} target='_blank' className={styles.githubLink}>
+                                        <HiMiniCodeBracket />
+                                        Ver código-fonte
+                                    </Link>
+                                : null}
+                            </div>
+
+                            <div>
+                                <button type="button" className={styles.closeBtn} onClick={() => requestClose()}>
+                                    <HiMiniXMark />
+                                    Fechar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+        </div>
+    );
+}
--- src/secions/ProjectsSection.tsx
+++ src/secions/ProjectsSection.tsx
@@ -1,183 +1,23 @@
-import Image from 'next/image';
 import styles from '../styles/sections/Projects.module.scss';
 import SectionTitle from '../components/SectionTitle';
-import { HiMiniCodeBracket, HiMiniXMark, HiMiniGlobeAlt } from "react-icons/hi2";
-import { AiOutlineFullscreen } from "react-icons/ai";
 import { useState } from 'react';
-import { isDesktop } from 'react-device-detect';
 
-import { Modal } from '../components/Modal';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { technologies } from '../utils/technologies';
+import { Project } from '../components/Project/Project';
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import { Technology, technologies } from '../utils/technologies';
-import Link from 'next/link';
-
-type ImageProps = {
-    src: string;
-    size: number[]; // [0]: width; [1]: height
-}
-
-type ProjectProps = {
-    img: string;
-    name: string;
-    demoLink?: string;
-    githubLink?: string;
-
-    imgAlign?: 'center' | 'top' | 'bottom' | number;
-    mobile?: boolean; // Na vitrine, imagens ocupam duas linhas e, no modal, imagens ficam na lateral
-    butDesktopShowcase?: boolean; // Se mobile=true e butDesktopShowcase=true, não exibe a imagem da vitrine em duas linhas
-
-    // For modal
-    imgs: ImageProps[];
-    description: string;
-    technologies: Technology[];
-    intention?: string;
-}
-
-const Project: React.FC<ProjectProps> = ({
-    img, name, demoLink, githubLink, imgAlign = 'center', mobile, butDesktopShowcase, imgs, description, technologies, intention
-}) =>
+const ProjectsSection: React.FC = () =>
 {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [closing, setClosing] = useState<boolean>(false);
-
-    function requestClose()
-    {
-        setClosing(true);
-
-        setTimeout(() => {
-            setClosing(false);
-            setIsOpen(false);
-        }, 300);
-    }
-
-    const getImgAlign = () =>
-        imgAlign === 'center' ? '50%' :
-        imgAlign === 'top' ? '0%' :
-        imgAlign === 'bottom' ? '100%' :
-        `${imgAlign}%`;
+    const [moreProjects, setMoreProjects] = useState(false);
 
     return (
-        <div
-            className={`${styles.project} ${isDesktop ? styles.project__isDesktop : ''}`}
-            style={
-                mobile && !butDesktopShowcase ? { gridRow: 'span 2' } : null
-            }
-        >
-            <div className={styles.imgContainer}>
-                <div className={styles.img} style={{
-                    backgroundImage: `url('/img/projects/${img}')`,
-                    backgroundPositionX: '50%',
-                    backgroundPositionY: getImgAlign()
-                }} />
-            </div>
-
-            <div
-                className={styles.caption}
-                onClick={() => setIsOpen(true)}
-            >
-                <span>{ name } <AiOutlineFullscreen /></span>
-                <span>Clique para ver detalhes</span>
-            </div>
-
-            <Modal
-                isOpen={isOpen}
-                onRequestClose={requestClose}
-                overlayClass={`${styles.modalOverlay} ${mobile ? styles.modalOverlay__Mobile : ''} ${closing ? styles.modalOverlay__Closing : ''} ${isOpen ? styles.modalOverlay__Open : ''}`}
-                containerClass={styles.modalContainer}
-            >
-                <div className={styles.modalContent}>
-                    <div className={styles.carouselContainer}>
-                        <Carousel className={styles.carousel} showThumbs={false}>
-                            {imgs.map(img =>
-                                <Image
-                                    key={img.src}
-                                    className={styles.carousel__item}
-                                    src={`/img/projects/${img.src}`}
-                                    width={img.size[0] ? img.size[0] : 10}
-                                    height={img.size[1] ? img.size[1] : 10}
-                                    layout='intrinsic'
-                                    alt={`${name} image`}
-                                    title={`${name} image`}
-                                />
-                            )}
-                        </Carousel>
-                    </div>
-
-                    <div className={styles.data}>
-                        <div className={styles.header}>
-                            <h3>{ name }</h3>
-                            <div className={styles.technologies}>
-                                {technologies.map((t, i) =>
-                                    <Image
-                                        key={i}
-                                        src={`/img/abilities/${t.imageName}.${t.extension ? t.extension : 'svg'}`}
-                                        // layout='intrinsic'
-                                        width={35}
-                                        height={35}
-                                        alt={t.name}
-                                        title={t.name}
-                                    />    
-                                )}
-                            </div>
-                        </div>
-
-                        <div className={styles.modalInfo}>
-                            <div className={styles.description}>
-                                {description.split('\n').map((desc, i) =>
-                                    <p key={i}>{ desc }</p>
-                                )}
-                            </div>
-
-                            {intention ?
-                                <div className={styles.intention}>
-                                    <span>Intúito: </span>
-                                    <span>{ intention }</span>
-                                </div>
-                            : null}
-                        </div>
-
-                        <div className={styles.projectLinks}>
-                            <div>
-                                {demoLink ?
-                                    <Link href={demoLink} target='_blank' className={styles.demoLink}>
-                                        <HiMiniGlobeAlt />
-                                        Visitar
-                                    </Link>
-                                : null}
-
-                                {githubLink ? 
-                                    <Link href={githubLink} target='_blank' className={styles.githubLink}>
-                                        <HiMiniCodeBracket />
-                                        Ver código-fonte
-                                    </Link>
-                                : null}
-                            </div>
-
-                            <div>
-                                <button type="button" className={styles.closeBtn} onClick={() => requestClose()}>
-                                    <HiMiniXMark />
-                                    Fechar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Modal>
-        </div>
-    );
-}
-
-const ProjectsSection2: React.FC = () =>
-{
-    return (
+        // height: '100svh',
         <div id="projects" className={`section ${styles.projectsSection}`}>
             <div className={styles.projectsContainer}>
 
                 <SectionTitle title='Portifólio' subtitle='Meus projetos' dark />
 
-                <div className={styles.projects}>
+                <div className={`${styles.projects} ${moreProjects ? styles.projects__moreProjects : ''}`}>
                     <Project
                         img='ai-chat/ai-chat-1-dark.png'
                         imgAlign='top'
@@ -313,6 +153,23 @@ const ProjectsSection2: React.FC = () =>
                         intention='Aprendizado'
                     />
                     <Project
+                        img='marvel-ttt/marvel-ttt-game.png'
+                        name='Marvel Tic Tac Toe'
+                        demoLink='https://prova-celero.vercel.app/'
+                        githubLink='https://github.com/bruno-remeikis/prova-celero'
+                        imgs={[
+                            { src: 'marvel-ttt/marvel-ttt-home.png', size: [1920, 1079] },
+                            { src: 'marvel-ttt/marvel-ttt-game.png', size: [1920, 1079] }
+                        ]}
+                        description=''
+                        technologies={[
+                            technologies.angular,
+                            technologies.sass
+                        ]}
+                        intention='Prova admissional'
+                    />
+                    <Project
+                        lastVisibleOne
                         img='portfolio/portifolio.png'
                         name='Este portifólio'
                         demoLink='https://portifolio-remeikis.vercel.app/'
@@ -331,9 +188,13 @@ const ProjectsSection2: React.FC = () =>
                     />
                 </div>
 
+                <div className={styles.viewMoreContainer} style={{ display: moreProjects ? 'none' : 'flex' }}>
+                    <button onClick={() => setMoreProjects(true)}>Mais projetos</button>
+                </div>
+
             </div>
         </div>
     );
 }
 
-export default ProjectsSection2;
+export default ProjectsSection;
